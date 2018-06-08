20f7357f3919c7a905b49739d7a1094f7044027b
Asad Hasen <asadhasan180@hotmail.com>
2018-06-08 17:38:19 +0100

    updated Thens regex, wip

--- src/test/java/org/asad/automation/steps/searchProducts/Thens.java
+++ src/test/java/org/asad/automation/steps/searchProducts/Thens.java
@@ -2,10 +2,13 @@ package org.asad.automation.steps.searchProducts;
 
 import cucumber.api.PendingException;
 import cucumber.api.java8.En;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 public class Thens implements En {
     public Thens() {
-        Then("^only results with <category> and <price> are shown$", () -> {
+        Then("^only results with (.+) and (.+) are shown$", (String category, String price) -> {
             // Write code here that turns the phrase above into concrete actions
             throw new PendingException();
         });
